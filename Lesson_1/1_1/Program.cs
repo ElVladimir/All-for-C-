4ae185abd78c79228170b6c5af034cfc451ed186
El Vladimir <hobodancer@icloud.com>
2022-06-25 16:02:51 +0300

    вторая задача боль

--- Lesson_1/1_1/Program.cs
+++ Lesson_1/1_1/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Где деньги, Лебовски?");
+﻿string num_string = Console.ReadLine();
+int num = int.Parse(num_string);
+
+
+Console.WriteLine($"{num}^2 = {num*num}");
---
+++ Lesson_1/1_2/Program.cs
@@ -0,0 +1,14 @@
+﻿string a = Console.ReadLine();
+string b = Console.ReadLine();
+
+int number1 = int.Parse(a);
+int number2 = int.Parse(b);
+
+if (number2==number1*number1)
+{
+    Console.WriteLine("Каеф пограммирование");
+}
+else
+{
+    Console.WriteLine("Вкушаем ущербие");
+}
