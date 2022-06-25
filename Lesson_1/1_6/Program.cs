5b2c3bd6e1ae3c77cf50b9c8b6ddf5be796de63e
El Vladimir <hobodancer@icloud.com>
2022-06-25 17:55:22 +0300

    задача с модулем

---
+++ Lesson_1/1_6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число:\n");
+int mod = int.Parse(Console.ReadLine());
+if (mod < 0){
+    Console.WriteLine($"Результат: {-mod}");
+}
+else if(mod > 0){
+    Console.WriteLine($"Результат: {mod}");
+}
+return;
