70a82f1c08dc108fa2e039009387e45e3797ccdc
El Vladimir <hobodancer@icloud.com>
2022-06-25 18:34:08 +0300

    ДЗ 1

---
+++ Lesson_1/DZ_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число\n");
+int a  = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число\n");
+int b = int.Parse(Console.ReadLine());
+if(a < b){
+    Console.WriteLine($"Самое большое число: {b}");
+}
+else if (a > b){
+    Console.WriteLine($"Самое большое число: {a}");
+}
+else if (a == b)
+{
+    Console.WriteLine($"Выпей-ка яду и съешь еще этих мягких французских булок");
+}
