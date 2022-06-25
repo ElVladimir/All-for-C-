5e384a296565666a97ee536acbb807db0d8a398b
El Vladimir <hobodancer@icloud.com>
2022-06-25 19:04:29 +0300

    ДЗ 2

---
+++ Lesson_1/DZ_2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите первое число:\n");
+int a  = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число:\n");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число:\n");
+int c = int.Parse(Console.ReadLine());
+if(a > b && a > c){
+    Console.WriteLine($"Самое большое число: {a}");
+}
+else if (a < b && b > c){
+    Console.WriteLine($"Самое большое число: {b}");
+}
+else if (a < b && b < c)
+{
+    Console.WriteLine($"Самое большое число: {c}");
+
+}
+
+//супермегаальфаговнокодdetected
+
+//подсмотрел у модных посонов что можно логическое ИЛИ запихать в код и будет счастье
+
+//но с подключением библиотек типа Math.Min и Math.Math все конечно веселее идет, но это уже совсем другая история
+ 
