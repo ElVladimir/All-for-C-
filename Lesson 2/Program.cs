7a7c0666cd403f81a2b5e2b3874d21c2546e74c5
El Vladimir <hobodancer@icloud.com>
2022-06-30 21:16:44 +0300

    метод заполнения массива

---
+++ Lesson 2/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = { 1, 12 , 35, 85, 4, 83, 16, 81};
+int n = array.Length;
+int find = 4;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
